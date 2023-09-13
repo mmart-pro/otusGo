6c34d7797db1d9ff6e7b68a5b3ea8e3baf9851be
mmart-pro <mmart@inbox.ru>
2023-09-13 19:21:01 +0300

    hw05_parallel_execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,90 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+type errorState struct {
+	mu           sync.Mutex
+	errorCount   int
+	errorLimit   int
+	errorChannel chan struct{}
+}
+
+func newRunState(maxErrors int) *errorState {
+	return &errorState{
+		errorLimit:   maxErrors,
+		errorChannel: make(chan struct{}),
+	}
+}
+
+func (r *errorState) incErrorCnt() {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.errorCount++
+	if r.errorCount == r.errorLimit {
+		close(r.errorChannel)
+	}
+}
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+var (
+	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+	wg                     sync.WaitGroup
+	state                  *errorState
+)
+
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	state = newRunState(m)
+	tasksChannel := make(chan Task, n)
+
+	// producer
+	go func() {
+		defer wg.Done()
+		defer close(tasksChannel)
+
+		for i := 0; i < len(tasks); {
+			select {
+			case <-state.errorChannel:
+				return
+			default:
+				if cap(tasksChannel) > len(tasksChannel) {
+					tasksChannel <- tasks[i]
+					i++
+				}
+			}
+		}
+	}()
+
+	// consumers
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case <-state.errorChannel:
+					return
+				default:
+					task, ok := <-tasksChannel
+					if !ok {
+						return
+					}
+
+					if err := task(); err != nil {
+						state.incErrorCnt()
+					}
+				}
+			}
+		}()
+	}
+
+	wg.Add(n + 1)
+	wg.Wait()
+
+	if state.errorLimit > 0 && state.errorCount >= state.errorLimit {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -15,6 +15,29 @@ import (
 func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
+	t.Run("run tasks with M = 0 should ignore errors ", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+	})
+
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
