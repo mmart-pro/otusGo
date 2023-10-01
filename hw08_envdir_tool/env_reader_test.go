3e91b1d47be2b0dae30d1e9349de4ad4f9e365c3
mmart-pro <mmart@inbox.ru>
2023-10-01 22:06:39 +0300

    hw_08

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,29 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadDirFailed(t *testing.T) {
+	t.Run("Error if not exists", func(t *testing.T) {
+		_, err := ReadDir("testdata/-env")
+		require.Error(t, err)
+	})
+}
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("Testdata expected result", func(t *testing.T) {
+		result, err := ReadDir("testdata/env")
+		require.NoError(t, err)
+		expected := Environment{
+			"BAR":   EnvValue{Value: "bar"},
+			"EMPTY": EnvValue{Value: ""},
+			"FOO":   EnvValue{Value: "   foo\nwith new line"},
+			"HELLO": EnvValue{Value: `"hello"`},
+			"UNSET": EnvValue{NeedRemove: true, Value: ""},
+		}
+		require.Equal(t, expected, result)
+	})
 }
