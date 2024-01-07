ecde2d042e470af36b1f7740f75879475357f0b3
Mo <mmart@inbox.ru>
2024-01-07 16:11:27 +0300

    add logger

--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -1,20 +1,57 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"time"
 
-type Logger struct { // TODO
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Logger struct {
+	logger *zap.SugaredLogger
 }
 
-func New(level string) *Logger {
-	return &Logger{}
+func NewLogger(level, logfile string) (*Logger, error) {
+	config := zap.NewProductionConfig()
+	if logfile != "" {
+		config.OutputPaths = append(config.OutputPaths, logfile)
+	}
+	config.DisableStacktrace = true
+	config.EncoderConfig.EncodeTime = func(t time.Time, encoder zapcore.PrimitiveArrayEncoder) {
+		encoder.AppendString(t.Format(time.RFC3339))
+	}
+
+	zlevel, err := zap.ParseAtomicLevel(level)
+	if err != nil {
+		return nil, fmt.Errorf("unknown level %s: %w", level, err)
+	}
+	config.Level = zlevel
+
+	zlogger, err := config.Build()
+	if err != nil {
+		return nil, fmt.Errorf("can't build logger: %w", err)
+	}
+
+	return &Logger{logger: zlogger.Sugar()}, nil
 }
 
-func (l Logger) Info(msg string) {
-	fmt.Println(msg)
+func (l Logger) Debugf(msg string, args ...interface{}) {
+	l.logger.Debugf(msg, args...)
 }
 
-func (l Logger) Error(msg string) {
-	// TODO
+func (l Logger) Infof(msg string, args ...interface{}) {
+	l.logger.Infof(msg, args...)
 }
 
-// TODO
+func (l Logger) Errorf(msg string, args ...interface{}) {
+	l.logger.Errorf(msg, args...)
+}
+
+func (l Logger) Fatalf(msg string, args ...interface{}) {
+	l.logger.Fatalf(msg, args...)
+}
+
+func (l Logger) Close() {
+	l.logger.Sync()
+}
--- hw12_13_14_15_calendar/internal/logger/logger_test.go
+++ hw12_13_14_15_calendar/internal/logger/logger_test.go
@@ -1,7 +1,65 @@
 package logger
 
-import "testing"
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
 
-func TestLogger(t *testing.T) {
-	// TODO
+	"github.com/stretchr/testify/require"
+)
+
+func TestLogLevels(t *testing.T) {
+	t.Run("test wrong", func(t *testing.T) {
+		l, err := NewLogger("wrong", "")
+		require.Error(t, err)
+		require.Nil(t, l)
+	})
+
+	t.Run("test ok", func(t *testing.T) {
+		test := []string{"debug", "info", "error", "fatal"}
+		for _, tc := range test {
+			l, err := NewLogger(tc, "")
+			require.NoError(t, err)
+			require.NotNil(t, l)
+			defer l.Close()
+		}
+	})
+}
+
+func TestLogWrite(t *testing.T) {
+	const (
+		NOT_EXISTS string = "NOT EXISTS"
+		EXISTS     string = "MESSAGE LOGGED"
+	)
+
+	str, err := captureStderr(func() {
+		log, err := NewLogger("info", "")
+		require.NoError(t, err)
+		defer log.Close()
+		log.Debugf(NOT_EXISTS)
+		log.Infof(EXISTS)
+	})
+	require.NoError(t, err)
+
+	require.False(t, strings.Contains(str, NOT_EXISTS))
+	require.True(t, strings.Contains(str, EXISTS))
+	require.True(t, strings.Contains(str, `"level":"info"`))
+}
+
+func captureStderr(f func()) (string, error) {
+	oldStdErr := os.Stderr
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		return "", err
+	}
+	os.Stderr = writer
+	f()
+	os.Stderr = oldStdErr
+	writer.Close()
+	out, err := io.ReadAll(reader)
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
 }
